bf287daf5b724f28b6fc09a6af44cf8f4bdde92e
Dean Radcliffe <chicagogrooves@gmail.com>
2011-03-20 15:21:18 -0500

    Fixed a reloading problem in dev mode, and moduled it up a bit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  include Apartment::ControllerDetection
   protect_from_forgery
 end
--- config/initializers/apartment_hooks.rb
+++ config/initializers/apartment_hooks.rb
@@ -3,7 +3,7 @@ class Apartment
     alias :really_set_current= :current=
     # Does the actual ActiveRecord magic after the setting of the apartment
     def current= apt
-      # $stderr.puts "ApartmentHooks -> Apartment#current"
+      $stderr.puts "ApartmentHooks -> Apartment#current"
       self.really_set_current = apt
       
       tenants.each do |t|
@@ -18,18 +18,28 @@ class Apartment
       end
     end
   end
+  
+  module ControllerDetection
+    def self.included(controller)
+      controller.class_eval do
+        prepend_before_filter :set_apartment
+
+        def set_apartment
+          logger.debug "ApplicationController#set_apartment"
+          apt = Apartment.from_request(request) 
+          Apartment.current = apt
+        rescue Apartment::InvalidError => ex
+          logger.warn "Could not create valid apartment. Error was: #{ex.message}. Request:#{[request.host, request.path, request.params]}"
+          raise "Suspected request tampering. Landlord says: Could not create valid Apartment."
+        end
+      end
+    end
+  end
 end
 
 ApplicationController.class_eval do
-  # you can do this where you want in the lifecycle, Dispatcher would be a good place, its 
-  # only a filter to illustrate it should happen early
-  prepend_before_filter :set_apartment
+  # Note code here is not automatically reloaded in dev mode - so for development mode to work
+  # you actually need include Apartment::ControllerDetection in the controllers class def
 
-  def set_apartment
-    apt = Apartment.from_request(request) 
-    Apartment.current = apt
-  rescue Apartment::InvalidError => ex
-    logger.warn "Could not create valid apartment. Error was: #{ex.message}. Request:#{[request.host, request.path, request.params]}"
-    raise "Suspected request tampering. Landlord says: Could not create valid Apartment."
-  end
+  include Apartment::ControllerDetection
 end
